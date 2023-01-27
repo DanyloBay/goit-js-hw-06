d0375e9dc3c3d0809cff3490b07eef19149c4146
Danylo Bay <danylobay@gmail.com>
2023-01-27 14:01:16 +0100

    task_2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listOfIngredients = document.querySelector('#ingredients');
+
+ingredients.forEach(item => {
+  const createListOfIngredient = document.createElement('li');
+  createListOfIngredient.classList.add('item');
+  createListOfIngredient.textContent = item;
+  listOfIngredients.appendChild(createListOfIngredient);
+});
