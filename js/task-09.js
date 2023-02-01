da779d4e75a29b1797847320e16c9f761b6c7211
Danylo Bay <danylobay@gmail.com>
2023-02-01 19:53:35 +0100

    task_9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const button = document.querySelector('.change-color');
+const textColorSpan = document.querySelector('.color');
+
+button.addEventListener('click', changeColor);
+
+function changeColor() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  textColorSpan.textContent = color;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
